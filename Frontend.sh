6734e7095fa9c6d3f94435695592a879af15c839
Jyothsnashrey <jyothsna.shrey@gmail.com>
2023-12-31 02:31:27 -0600

    Added echo commands to backend

--- Frontend.sh
+++ Frontend.sh
@@ -10,20 +10,20 @@ echo $?
 Head "Copy Expense.conf file"
 cp expense.conf  /etc/nginx/default.d/expense.conf &>>log_file
 echo $?
-head "Removing default content"
+Head "Removing default content"
 rm -rf /usr/share/nginx/html/* &>>log_file
 echo $?
-head "Downloading Application Code"
+Head "Downloading Application Code"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
 echo $?
 
 cd /usr/share/nginx/html
 
-head "Extract Application"
+Head "Extract Application"
 unzip /tmp/frontend.zip &>>log_file
 echo $?
 
-head "start Nginx service"
+Head "start Nginx service"
 systemctl enable nginx &>>log_file
 systemctl restart nginx &>>log_file
 echo $?
