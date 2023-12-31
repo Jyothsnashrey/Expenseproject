9e68e65bfe0cc46812e6d3f66d1a82e72d4fa1e0
Jyothsnashrey <jyothsna.shrey@gmail.com>
2023-12-31 00:48:33 -0600

    Added echo commands to backend

--- backend.sh
+++ backend.sh
@@ -1,38 +1,38 @@
 
-echo disable deafault version of nodejs
+echo -e "\e[35mdisable default version of nodejs\e[0m"
 dnf module disable nodejs -y
 
-echo enable node js version
+echo -e "\e[35menable node js version\e[0m"
 dnf module enable nodejs:18 -y
 
-echo Install nodejs
+echo -e "\e[35mInstall nodejs\e[0m"
 dnf install nodejs -y
 
-echo Configure backend service
+echo -e "\e[35mConfigure backend service\e[0m"
 cp  backend.service /etc/systemd/system/backend.service
 
-echo Adding application user
+echo -e "\e[35mAdding application user\e[0m"
 useradd expense
 
-echo Remove existing App content
+echo -e "\e[35mRemove existing App content\e[0m"
 rm -rf /app
 
-echo Create Application Directory
+echo -e "\e[35mCreate Application Directory\e[0m"
 mkdir /app
 
-echo download Application content
+echo -e"\e[35m download Application content\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 
-echo Extracting application content
+echo -e"\e[35m Extracting application content\e[0m"
 
 unzip /tmp/backend.zip
 
-echo Downloading application content
+echo -e"\e[35m Downloading application content\e[0m"
 npm install
 
 
-echo Reloading application content and restarting service
+echo -e"\e[35m Reloading application content and restarting service\e[0m"
 
 systemctl daemon-reload
 
@@ -40,7 +40,7 @@ systemctl enable backend
 systemctl start backend
 systemctl restart backend
 
-echo Install mysql client
+echo -e "\e[35m Install mysql client\e[0m"
 
 dnf install mysql -y
 
