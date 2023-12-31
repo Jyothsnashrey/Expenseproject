0b25d701109752d2515f4e22ea274c1b9dc1b2cc
Jyothsnashrey <jyothsna.shrey@gmail.com>
2023-12-31 02:28:48 -0600

    Added echo commands to backend

--- Frontend.sh
+++ Frontend.sh
@@ -1,9 +1,29 @@
-dnf install nginx -y
-cp expense.conf  /etc/nginx/default.d/expense.conf
+Log_file=/tmp/expense.log
 
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+Head()
+{
+ echo -e "\e[35m$1\e[0m"
+}
+Head "Installing Nginx"
+dnf install nginx -y &>>log_file
+echo $?
+Head "Copy Expense.conf file"
+cp expense.conf  /etc/nginx/default.d/expense.conf &>>log_file
+echo $?
+head "Removing default content"
+rm -rf /usr/share/nginx/html/* &>>log_file
+echo $?
+head "Downloading Application Code"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+echo $?
+
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+
+head "Extract Application"
+unzip /tmp/frontend.zip &>>log_file
+echo $?
+
+head "start Nginx service"
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
+echo $?
--- backend.sh
+++ backend.sh
@@ -7,36 +7,36 @@ Head()
 }
 Head "disable default version of nodejs"
 dnf module disable nodejs -y &>>log_file
-
+echo $?
 Head "enable node js version"
 dnf module enable nodejs:18 -y &>>log_file
-
+echo $?
 Head "Install nodejs"
 dnf install nodejs -y &>>log_file
-
+echo $?
 Head "Configure backend service"
 cp  backend.service /etc/systemd/system/backend.service &>>log_file
-
+echo $?
 Head "Adding application user"
 useradd expense &>>log_file
-
+echo $?
 Head "Remove existing App content"
 rm -rf /app &>>log_file
-
+echo $?
 Head "Create Application Directory"
 mkdir /app &>>log_file
-
+echo $?
 Head "download Application content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
 cd /app &>>log_file
-
+echo $?
 Head "Extracting application content"
 
 unzip /tmp/backend.zip &>>log_file
-
+echo $?
 Head "Downloading application content"
 npm install &>>log_file
-
+echo $?
 
 Head "Reloading application content and restarting service"
 
@@ -45,11 +45,11 @@ systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
 systemctl restart backend &>>log_file
-
+echo $?
 Head "Install mysql client"
 
 dnf install mysql -y &>>log_file
-
+echo $?
 mysql -h mysql-dev.jyothsnashrey.online -uroot -p${Mysql_Password} < /app/schema/backend.sql
-
+echo $?
 #Mysql password ExpenseApp@1
