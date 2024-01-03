2190f2c1cbcee44c92d5a59f3eec91d4db98b670
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-01-03 12:08:58 -0600

    Added if else commands to backend

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,8 @@
 Mysql_Password=$1
+if [ -z "Mysql_Password" ]; then
+  echo Input Mysql_Password is missing
+  exit 1
+  fi
 dnf module disable mysql -y
 cp  mysql.repo /etc/yum.repos.d/mysql.repo
 dnf install mysql-community-server -y
